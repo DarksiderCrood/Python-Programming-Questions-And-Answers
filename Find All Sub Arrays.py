f677246985c0664788a5e2ea3475b18bcd8e4a45
DarksiderCrood <zexerfist02@gmail.com>
2021-08-06 00:04:56 +0530

    5 Aug 2021

--- Find All Sub Arrays.py
+++ Find All Sub Arrays.py
@@ -1,10 +1,11 @@
-lst = [1,2,3]
-size = len(lst)
-item = 0
+def sub_lists(arr): 
+    sub_list = [[]]  # Store all the sublists 
 
-result = [sublist for sublist in 
-        (lst[x:x+size] for x in range(len(lst) - size + 1))
-        if item not in sublist
-    ]
-
-print(result)
+    for i in range(len(arr) + 1):        
+        for j in range(i + 1, len(arr) + 1): 
+            sub = arr[i:j] 
+            sub_list.append(sub)  
+    return sub_list 
+  
+arr = [4,5,8,4,8,5,2,58,486] 
+print(sub_lists(arr))
