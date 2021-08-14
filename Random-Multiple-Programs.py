779f7adae2e490a0a0f07255581e897dc49c34d9
DarksiderCrood <zexerfist02@gmail.com>
2021-08-14 14:37:47 +0530

    12 Aug 2021

---
+++ Random-Multiple-Programs.py
@@ -0,0 +1,20 @@
+# Exercise 1: Reverse a given list in Python
+'''aLsit = [100, 200, 300, 400, 500]
+print(aLsit[::-1])'''
+
+# Exercise 2: Concatenate two lists index-wise ['My', 'name', 'is', 'Kelly']
+'''list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+list3 = [x+y for x,y in zip(list1,list2)]
+print(list3)'''
+
+# Exercise 3: Given a Python list of numbers. Turn every item of a list into its square ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+'''aList = [1, 2, 3, 4, 5, 6, 7]
+lst = [x**2 for x in aList]
+print(lst)'''
+
+# Exercise 4: Concatenate two lists in the following order
+'''list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+list3 = [x+y for x in list1 for y in list2]
+print(list3)'''
