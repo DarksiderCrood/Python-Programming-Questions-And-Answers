c81f50aef12a1027e1e633e9f9e21ac61f598ede
DarksiderCrood <jhakaushtubh@gmail.com>
2021-08-19 21:38:09 +0530

    19 Aug 2021

--- OOP-Exercises.py
+++ OOP-Exercises.py
@@ -1,8 +1,26 @@
 # Exercise 1: Create a Vehicle class with max_speed and mileage instance attributes
-class Vehicle:
+'''class Vehicle:
     def __init__(self, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
 
 model = Vehicle(240, 18)
-print(model.max_speed, model.mileage)
+print(model.max_speed, model.mileage)'''
+
+# Exercise 2: Create a Vehicle class without any variables and methods
+'''class NoVarMeth:
+    pass'''
+
+# Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+'''class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+bus_obj = Bus('Volvo', 210, 40)
+print(bus_obj.name, bus_obj.max_speed, bus_obj.mileage)
+'''
