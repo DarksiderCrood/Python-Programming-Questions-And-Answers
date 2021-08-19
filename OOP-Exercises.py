4daf83eebe2cace26f38e8ee3f909016d865413f
DarksiderCrood <jhakaushtubh@gmail.com>
2021-08-19 21:29:50 +0530

    19 Aug 2021

--- OOP-Exercises.py
+++ OOP-Exercises.py
@@ -1,1 +1,8 @@
 # Exercise 1: Create a Vehicle class with max_speed and mileage instance attributes
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+model = Vehicle(240, 18)
+print(model.max_speed, model.mileage)
