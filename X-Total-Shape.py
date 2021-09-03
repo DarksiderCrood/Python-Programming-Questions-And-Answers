c12197b93f17978385a4db9b910c7e22a486d06d
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-03 22:17:08 +0530

    3 Sep 2021

--- X-Total-Shape.py
+++ X-Total-Shape.py
@@ -48,39 +48,38 @@ Constraints:
 
 class Solution:
     def xShape(self, grid):
-        n=len(grid)
-        m=len(grid[0])
-        mark=[[False for j in range(m)] for i in range(n)]
-        dr=[[0,1],[0,-1],[1,0],[-1,0]]
-        ans=0
-        for i in range(0,n) :
-            for j in range(0,m) :
-                if grid[i][j]=='X' and mark[i][j]==False :
-                    ans+=1
-                    mark[i][j]=True
-                    q=[[i,j]]
-                    while(len(q)>0) :
-                        f=q[0]
-                        q.pop(0)
-                        #print(f[0]," ",f[1])
+        normal=len(grid)
+        max_stage=len(grid[0])
+        check=[[False for j in range(max_stage)] for i in range(normal)]
+        destination=[[0,1],[0,-1],[1,0],[-1,0]]
+        rlst=0
+        for i in range(0,normal) :
+            for j in range(0,max_stage) :
+                if grid[i][j]=='X' and check[i][j]==False :
+                    rlst+=1
+                    check[i][j]=True
+                    random_num=[[i,j]]
+                    while(len(random_num)>0) :
+                        quick_fold=random_num[0]
+                        random_num.pop(0)
                         for k in range(0,4) :
-                            x=f[0]+dr[k][0]
-                            y=f[1]+dr[k][1]
-                            if x>=0 and x<n and y>=0 and y<m and grid[x][y]=='X' and mark[x][y]==False :
-                                mark[x][y]=True
-                                q.append([x,y])          
-        return ans
+                            x_axis=quick_fold[0]+destination[k][0]
+                            y_axis=quick_fold[1]+destination[k][1]
+                            if x_axis>=0 and x_axis<normal and y_axis>=0 and y_axis<max_stage and grid[x_axis][y_axis]=='X' and check[x_axis][y_axis]==False:
+                                check[x_axis][y_axis]=True
+                                random_num.append([x_axis,y_axis])          
+        return rlst
 
 
 if __name__ == '__main__':
     T=int(input())
     for i in range(T):
-        n, m = map(int, input().split())
-        grid = [['#' for i in range(m)] for j in range(n)]
-        for i in range(n):
+        normal, max_stage = map(int, input().split())
+        grid = [['#' for i in range(max_stage)] for j in range(normal)]
+        for i in range(normal):
             s = input().strip()
-            for j in range(m):
+            for j in range(max_stage):
                 grid[i][j] = s[j]
         obj = Solution()
-        ans = obj.xShape(grid)
-        print(ans)
+        result = obj.xShape(grid)
+        print(result)
