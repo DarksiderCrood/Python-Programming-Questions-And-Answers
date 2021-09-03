433eb5676396e40bbf0236397d90dd44d7e4c814
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-03 14:27:44 +0530

    3 Sep 2021

--- List-Questions.py
+++ List-Questions.py
@@ -75,22 +75,102 @@ for x in l:
 print(sums)'''
 
 # 2. Write a python program to find largest number in a given list with out using max()
-l = [3,6,1,3,8,9,3,4,5,6,9,1,3,5]
-print(max(l))
+'''l = [3,6,1,3,8,9,3,4,5,6,9,1,3,5]
+print(max(l)) # with max
+# or
+l.sort()
+print(l[len(l)-1]) # with sort and len
+# or
+max = 0
+for x in l:
+    if x > max:
+        max = x
+print(max)'''
+
 # 3. Write a python program to find the common numbers from two lists
+'''q = [1,3,5,7,9]
+w = [0,3,5,7,11]
+for x,y in zip(q,w):
+    if x == y:
+        print("Common Numbers are: ", x)
+# or
+a, s = set(q), set(w)
+print(a.intersection(s))'''
 
 # 4. Write a python program to print all even numbers from a given list
+'''l = [1,2,3,4,5,6,7,8,9,10]
+print([x for x in l if x%2==0])'''
 
 # 5. Write a python program to create a list of even numbers and another list of odd numbers from a given list
+'''l = [1,2,3,4,5,6,7,8,9,10]
+print([x for x in l if x%2==0])
+print([x for x in l if x%2!=0])'''
 
 # 6. Write a python program to remove repeated elements from a given list without using built-in methods
+'''l = [1,2,3,3,4,5,5,5,6,7,8,8,9,0,0]
+n = []
+for x in l:
+    if x not in n:
+        n.append(x)
+print(n)'''
 
 # 7. Write a python program to find the longest word in a given sentence
+'''l = ['qwe','asdff','DFDSFDg','zxcvxcvx','sdfsdfs']
+print(max(l))
+# or
+max = 0
+word = ''
+for x in l:
+    if len(x) > max:
+        max = len(x)
+        word = x
+print(word, max)'''
 
 # 8. Write a python program to find number of occurrences of given number with out using built-in methods
+'''l = [1,2,3,3,4,5,5,5,6,7,8,8,9,0,0]
+n = 5
+c = 0
+for x in l:
+    if x == n:
+        c+=1
+print(n,"comes in list {} times".format(c))'''
 
 # 9. ["www.zframez.com", "www.wikipedia.org", "www.asp.net", "www.abcd.in"]
-
 # Write a python program to print website suffixes (com , org , net ,in) from this list
+'''l = ["www.zframez.com", "www.wikipedia.org", "www.asp.net", "www.abcd.in"]
+for x in l:
+    print(x.split('.')[2])'''
 
 # 10. Write a python program to sort a given list of numbers with out using sort() function
+def bubbleSort(arr):
+	n = len(arr)
+
+	# Traverse through all array elements
+	for i in range(n):
+		swapped = False
+
+		# Last i elements are already
+		# in place
+		for j in range(0, n-i-1):
+
+			# traverse the array from 0 to
+			# n-i-1. Swap if the element
+			# found is greater than the
+			# next element
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = True
+
+		# IF no two elements were swapped
+		# by inner loop, then break
+		if swapped == False:
+			break
+		
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print ("Sorted array :")
+for i in range(len(arr)):
+	print ("%d" %arr[i],end=" ")
