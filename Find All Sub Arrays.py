937355eecefe229f8e2f0e89eeb74af40fef6392
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-04 00:12:43 +0530

    3 Sep 2021

--- Find All Sub Arrays.py
+++ Find All Sub Arrays.py
@@ -9,3 +9,13 @@ def sub_lists(my_list):
 
 arr = [1,2,3,4,5] 
 print(sub_lists(arr))
+
+# or
+
+l = [1,2,3]
+s = []
+for x in range(len(l)+1):
+    for y in range(x+1,len(l)+1):
+        r = l[x:y]
+        s.append(r)
+print(s)
