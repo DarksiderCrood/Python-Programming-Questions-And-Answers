4ca30a37165cb53b899cf0950f345347a5fac815
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-03 22:11:27 +0530

    3 Sep 2021

--- X-Total-Shape.py
+++ X-Total-Shape.py
@@ -46,3 +46,41 @@ Constraints:
 1 ≤ n, m ≤ 100
 '''
 
+class Solution:
+    def xShape(self, grid):
+        n=len(grid)
+        m=len(grid[0])
+        mark=[[False for j in range(m)] for i in range(n)]
+        dr=[[0,1],[0,-1],[1,0],[-1,0]]
+        ans=0
+        for i in range(0,n) :
+            for j in range(0,m) :
+                if grid[i][j]=='X' and mark[i][j]==False :
+                    ans+=1
+                    mark[i][j]=True
+                    q=[[i,j]]
+                    while(len(q)>0) :
+                        f=q[0]
+                        q.pop(0)
+                        #print(f[0]," ",f[1])
+                        for k in range(0,4) :
+                            x=f[0]+dr[k][0]
+                            y=f[1]+dr[k][1]
+                            if x>=0 and x<n and y>=0 and y<m and grid[x][y]=='X' and mark[x][y]==False :
+                                mark[x][y]=True
+                                q.append([x,y])          
+        return ans
+
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        n, m = map(int, input().split())
+        grid = [['#' for i in range(m)] for j in range(n)]
+        for i in range(n):
+            s = input().strip()
+            for j in range(m):
+                grid[i][j] = s[j]
+        obj = Solution()
+        ans = obj.xShape(grid)
+        print(ans)
