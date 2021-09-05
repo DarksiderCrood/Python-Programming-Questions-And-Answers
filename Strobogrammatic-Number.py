348824a3cb6d6e1521e9dca041a7de7598a0ed20
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-05 21:57:20 +0530

    5 Sep 2021

--- Strobogrammatic-Number.py
+++ Strobogrammatic-Number.py
@@ -1,23 +1,21 @@
 
-def LoopOver(n):    
-    temp = Strobo_Num(n, n)
+def LoopOver(num):    
+    temp = Strobo_Num(num, num)
     return temp
       
 
-def Strobo_Num(n, lent):
-    if n == 0: return [""]
-    if n == 1: return ["1", "0", "8"]
-      
-    digits = Strobo_Num(n - 2, lent)
+def Strobo_Num(num, lent):
+    if num == 0: return ['']
+    if num == 1: return ['0','1','8']
+    digits = Strobo_Num(num - 2, lent)
     temp = []
-      
     for digit in digits:
-        if n != lent:            
+        if num != lent:            
             temp.append("0" + digit + "0")
-        temp.append("8" + digit + "8")
-        temp.append("1" + digit + "1")
-        temp.append("9" + digit + "6")
-        temp.append("6" + digit + "9")
+        temp.append('1' + digit + '1')
+        temp.append('6' + digit + '9')
+        temp.append('8' + digit + '8')
+        temp.append('9' + digit + '6')
     return temp
   
 
