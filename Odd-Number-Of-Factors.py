77e738e22c8d9859bbecbf4d89336d58d0834734
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-07 23:05:58 +0530

    7 Sep 2021

--- Odd-Number-Of-Factors.py
+++ Odd-Number-Of-Factors.py
@@ -38,7 +38,6 @@ class Solution:
         count = 0
         for x in range(1, N+1):
             a = []
-            
             for y in range(1, x+1): 
                 if x%y==0:
                     a.append(y)
