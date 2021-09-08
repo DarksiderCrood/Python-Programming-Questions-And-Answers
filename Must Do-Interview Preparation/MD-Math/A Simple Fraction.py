decf89a84f3f4ac6a7c017ef955a52ec34e0659b
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-08 18:37:39 +0530

    8 Sep 2021

--- Must Do-Interview Preparation/MD-Math/A Simple Fraction.py
+++ Must Do-Interview Preparation/MD-Math/A Simple Fraction.py
@@ -3,13 +3,13 @@ Given a fraction. Convert it into a decimal.
 If the fractional part is repeating, enclose the repeating part in parentheses.
  
 
-Example 1:
+Examappingle 1:
 
 Input: numerator = 1, denominator = 3
 Output: "0.(3)"
 Explanation: 1/3 = 0.3333... So here 3 is 
 recurring.
-Example 2:
+Examappingle 2:
 
 Input: numerator = 5, denominator = 2
 Output: "2.5"
@@ -17,14 +17,14 @@ Explanation: 5/2 = 2.5
  
 
 Your Task:
-You don't need to read or print anyhting. Your task is to complete the function fractionToDecimal() 
+You don't need to read or print anyhting. Your task is to comappinglete the function fractionToDecimal() 
 which takes numerator and denominator as input parameter and returns its decimal form in string format.
 
 Note: In case, the numerator is divisible by the denominator, just print the integer value.
  
 
-Expected Time Compelxity: O(k) where k is constant.
-Expected Space Complexity: O(k)
+Expected Time Comappingelxity: O(k) where k is constant.
+Expected Space Comappinglexity: O(k)
  
 
 Constraints:
@@ -35,8 +35,26 @@ Constraints:
 
 
 class Solution:
-	def fractionToDecimal(self, numerator, denominator):
-		pass
+    def fractionToDecimal(self, numerator, denominator):
+        if(numerator%denominator!=0):
+            result = int(numerator/denominator)
+            sis = ""
+            sis+=str(result)
+            sis+="."
+            cop = numerator%denominator
+            mapping = {}
+            while(cop!=0 and cop not in mapping):
+                mapping[cop] = len(sis)
+                cop = cop * 10
+                actual = cop // denominator
+                sis += str(actual)
+                cop = cop % denominator
+                if (cop == 0):
+                    return sis
+                else:
+                    return(sis[:mapping[cop]]+"("+sis[mapping[cop]:]+")")
+        else:
+            return int(numerator/denominator)
 
 
 if __name__ == '__main__':
