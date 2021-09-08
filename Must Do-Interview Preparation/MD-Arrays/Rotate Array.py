bf6b3660e0bd44ec05ce10eeb2d709ca867c8664
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-08 19:17:15 +0530

    8 Sep 2021

--- Must Do-Interview Preparation/MD-Arrays/Rotate Array.py
+++ Must Do-Interview Preparation/MD-Arrays/Rotate Array.py
@@ -39,3 +39,32 @@ Constraints:
 0 <= arr[i] <= 105
 '''
 
+
+class Solution:
+    def rotateArr(self,A,D,N):
+        A.extend(A[:D])
+        A = A[D:]
+        return A
+
+
+import math
+def main():
+    T=int(input())
+    
+    while(T>0):
+        nd=[int(x) for x in input().strip().split()]
+        N=nd[0]
+        D=nd[1]
+        A=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.rotateArr(A,D,N)
+        
+        for i in A:
+            print(i,end=" ")
+            
+        print()
+       
+        T-=1
+
+if __name__=="__main__":
+    main()
