a149823aabb53e9b5dde119063388a4e83b9eecd
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-13 20:56:02 +0530

    13 Sep 2021

---
+++ Questions for the Day/Hard/String/Similar characters Strings comparison.py
@@ -0,0 +1,12 @@
+'''
+Given two Strings, separated by delim, check if both contain same characters.
+
+Input : test_str1 = ‘e!e!k!s!g’, test_str2 = ‘g!e!e!k!s’, delim = ‘!’
+Output : True
+Explanation : Same characters, just diff. positions.
+
+Input : test_str1 = ‘e!e!k!s’, test_str2 = ‘g!e!e!k!s’, delim = ‘!’
+Output : False
+Explanation : g missing in 1st String.
+'''
+
