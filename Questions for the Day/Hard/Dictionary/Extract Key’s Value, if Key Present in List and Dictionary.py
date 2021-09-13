eefcfe974406be864489f4b070d60d8b08827989
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-13 19:48:06 +0530

    13 Sep 2021

---
+++ Questions for the Day/Hard/Dictionary/Extract Key’s Value, if Key Present in List and Dictionary.py
@@ -0,0 +1,13 @@
+'''
+Given a list, dictionary and a Key K, print value of K from dictionary if 
+key present in both, list and dictionary.
+
+Input : test_list = [“Gfg”, “is”, “Good”, “for”, “Geeks”], test_dict = {“Gfg” : 5, “Best” : 6}, K = “Gfg”
+Output : 5
+Explanation : “Gfg” is present in list and has value 5 in dictionary.
+
+Input : test_list = [“Good”, “for”, “Geeks”], test_dict = {“Gfg” : 5, “Best” : 6}, K = “Gfg”
+Output : None
+Explanation : “Gfg” not present in List.
+'''
+
