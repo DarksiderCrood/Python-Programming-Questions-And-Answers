1c187256b1a1f648a0bc59ced4705a56a485fea4
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-15 21:42:18 +0530

    15 Sep 2021

--- Questions for the Day/Medium/Sum-of-sub-array.py
+++ Questions for the Day/Medium/Sum-of-sub-array.py
@@ -1,2 +1,11 @@
+from itertools import combinations
+
 l = [1,4,3,6,8,2,7,9,10]
 n = len(l)
+sub = []
+
+for i in range(0, n+1):
+    temp = [list(x) for x in combinations(l, i)]
+    if len(temp)<0:
+        sub.extend(temp)
+print(sub)
