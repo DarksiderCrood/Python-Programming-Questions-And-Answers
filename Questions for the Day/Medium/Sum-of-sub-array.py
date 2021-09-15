815d200a6fa12da8cae6ac34729d17c30d288e1a
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-15 21:50:01 +0530

    15 Sep 2021

--- Questions for the Day/Medium/Sum-of-sub-array.py
+++ Questions for the Day/Medium/Sum-of-sub-array.py
@@ -1,6 +1,6 @@
 from itertools import combinations
 
-l = [1,4,3]
+l = [1,4,3,5]
 n = len(l)
 sub = []
 
@@ -8,7 +8,4 @@ for i in range(0, n+1):
     temp = [list(x) for x in combinations(l, i)]
     if len(temp)>0:
         sub.extend(temp)
-print(sub)
-maxs = [len(z) for z in sub]
-
-print(max(sub))
+print(max(sum(x) for x in sub if len(x)!=n)) # not including sublist consisting all original list elements
