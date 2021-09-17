fda0eda6bd78d35e00d58d90ef542c9430c82d72
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-17 12:59:52 +0530

    17 Sep 2021

---
+++ Questions for the Day/Medium/Find_inde-query.py
@@ -0,0 +1,19 @@
+'''
+Aahad and Harshit always have fun by solving
+problems. Harshit took a sorted array and rotated it
+clockwise by an unknown amount. For example,
+he took a sorted array = [1, 2, 3, 4, 5] and if he rotates
+it by 2, then the array becomes: [4, 5, 1, 2, 3].
+After rotating a sorted array, Aahad needs to answer
+
+Q queries asked by Harshit, each of them is described
+by one integer Qi] which Harshit wanted him to search
+in the array. For each query, if he found it,
+he had to shout the index of the number, otherwise,
+he had to shout -1.
+
+Note:
+You are not required to explicitly print the expected
+output, just return it and printing has
+already been taken care of.
+'''
