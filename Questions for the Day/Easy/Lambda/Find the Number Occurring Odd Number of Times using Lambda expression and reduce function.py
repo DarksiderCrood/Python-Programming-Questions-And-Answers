eb4ab19c820b0624e0d9c78ddb612c13c6cabaef
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-18 12:08:06 +0530

    18 Sep 2021

---
+++ Questions for the Day/Easy/Lambda/Find the Number Occurring Odd Number of Times using Lambda expression and reduce function.py
@@ -0,0 +1,11 @@
+'''
+Find the Number Occurring Odd Number of Times using Lambda expression and reduce function
+
+Given an array of positive integers. All numbers occur even number of times 
+except one number which occurs odd number of times. Find the number in O(n) time & constant space.
+
+Examples:
+
+Input :  [1, 2, 3, 2, 3, 1, 3]
+Output :  3
+'''
