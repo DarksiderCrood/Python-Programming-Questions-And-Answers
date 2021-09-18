ba0d82d9c0d75a0e749c0f923f6227737db1eac6
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-18 12:14:36 +0530

    18 Sep 2021

--- Questions for the Day/Easy/Lambda/Find the Number Occurring Odd Number of Times using Lambda expression and reduce function.py
+++ Questions for the Day/Easy/Lambda/Find the Number Occurring Odd Number of Times using Lambda expression and reduce function.py
@@ -9,3 +9,11 @@ Examples:
 Input :  [1, 2, 3, 2, 3, 1, 3]
 Output :  3
 '''
+
+test_list = [1, 2, 3, 2, 3, 1, 3]
+
+occure = list(map(lambda x:test_list.count(x)!=0, test_list))
+print(occure)
+
+for x in test_list:
+    print(test_list.count(x))
