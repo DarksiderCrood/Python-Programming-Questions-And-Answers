c16b181ee4c717d58d14aab197d0af7b543f5aa0
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-19 11:55:26 +0530

    19 Sep 2021

--- Questions for the Day/Easy/Set/Difference between two lists.py
+++ Questions for the Day/Easy/Set/Difference between two lists.py
@@ -18,18 +18,30 @@ resultant list = list1 - list2
  In that case, this code will simply remove the same elements.
 In that case, you can maintain a count of each element in both lists.
 '''
-list1 = [10, 15, 20, 25, 30, 35, 40]
-list2 = [25, 40, 35] 
-print(set(list1).difference(set(list2)))
-
-# Brute Way
+list2 = [10, 15, 20, 25, 30, 35, 40]
+list1 = [25, 40, 35] 
 
 l1 = len(list1)
 l2 = len(list2)
 if l1 > l2:
-    for x in l2:
-        if x in l1:
-            l1.remove(x)
-    print(l1)
+    print(set(list1).difference(set(list2)))
 else:
-    pass
+    print(set(list2).difference(set(list1)))
+
+
+# Brute Way
+list2 = [10, 15, 20, 25, 30, 35, 40]
+list1 = [25, 40, 35] 
+
+l1 = len(list1)
+l2 = len(list2)
+if l1 > l2:
+    for x in list2:
+        if x in list1:
+            list1.remove(x)
+    print(list1)
+else:
+    for x in list1:
+        if x in list2:
+            list2.remove(x)
+    print(list2)
