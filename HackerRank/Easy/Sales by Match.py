32c1731568d92f380046851fa4931f8322cfd9e0
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-20 10:56:53 +0530

    20 Sep 2021

--- HackerRank/Easy/Sales by Match.py
+++ HackerRank/Easy/Sales by Match.py
@@ -24,3 +24,18 @@ if __name__ == '__main__':
     result = sockMerchant(n, ar)
     fptr.write(str(result) + '\n')
     fptr.close()
+
+
+# Raw Code
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+n = 9
+dc = {x:ar.count(x) for x in ar}
+print(dc)
+count = 0
+for x,y in dc.items():
+    if y%2!=0:
+        mod = y-1
+        count+=mod
+    else:
+        count+=y
+print(int(count/2))
