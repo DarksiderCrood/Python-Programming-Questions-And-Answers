c46c33ef7c73b6e89adda748f15a516253a259c1
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-22 18:08:09 +0530

    22 Sep 2021

--- test.py
+++ test.py
@@ -1,42 +1,35 @@
-'''
-Given two strings A and B, find the minimum number of times A has to be
-repeated such that B becomes a substring of the repeated A. If B cannot be a substring 
-of A no matter how many times it is repeated, return -1.
-
-Example 1:
-
-Input: A = "abcd", B = "cdabcdab"
-Output: 3
-Explanation: After repeating A three times, 
-we get "abcdabcdabcd".
-Example 2:
-
-Input: A = "aa", B = "a"
-Output: 1
-Explanation: B is already a substring of A.
-Your Task:  
-You don't need to read input or print anything. Complete the function repeatedStringMatch() 
-which takes strings A and B as input parameters and returns the minimum number of operations 
-to complete the task. If it is not possible then return -1.
-
-Expected Time Complexity: O(|A| * |B|)
-Expected Auxiliary Space: O(1)
-
-Constraints:
-1 ≤ |A|, |B| ≤ 103
-'''
-
-from itertools import combinations
-
-
 # Find Subs of A
 def A_Subs(A):
-    subs = []
-    for x in range(0, len(A)+1):
-        temp = [list(x) for x in combinations(A, x)]
-        if len(temp) > 0:
-            subs.extend(temp)
-    return subs
+    # code here
+    count = 1
+    while True:
+        if B in A:
+            return count
+            break
+        else:
+            count+=1
+            A = A+A
+    return -1
+
+# Other Approach
+'''
+    def repeatedStringMatch(self, A, B):
+        num=A
+        if A.count(B)>0:
+            return 1
+        else:
+            count=1
+            while( len(A)<len(B)):
+                A+=num
+                count+=1
+            if A.count(B)>0:
+                return count
+            A+=num
+            count+=1
+            if A.count(B)>0:
+                return count
+        return -1
+'''
 
 
 # Main
@@ -44,13 +37,7 @@ if __name__ == '__main__':
     # Inputs
     A = "abcd"
     B = "cdabcdab"
+    result = A_Subs(A)
+    print(result)
 
-    count = 1
-    while True:
-        if B in A:
-            print(count)
-            break
-        else:
-            count+=1
-            # result = A_Subs(A)
-            A = A+A
+
