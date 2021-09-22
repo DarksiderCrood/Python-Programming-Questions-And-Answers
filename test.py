69eed168f68e4af7fb8d7ec82bdd4a0d072f2db3
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-22 17:43:25 +0530

    22 Sep 2021

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-
-
-ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
-n = 9
-dc = {x:ar.count(x) for x in ar}
-print(dc)
-count = 0
-for x,y in dc.items():
-    if y%2!=0:
-        mod = y-1
-        count+=mod
-    else:
-        count+=y
-print(int(count/2))
+A = "abcd"
+B = "cdabcdab"
