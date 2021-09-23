193f6976d80100f9cfa292388958bbbcceacd007
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-24 00:32:04 +0530

    24 Sep 2021

--- Questions for the Day/Easy/DateTime/Convert-12hr-to-24hr.py
+++ Questions for the Day/Easy/DateTime/Convert-12hr-to-24hr.py
@@ -1,9 +1,20 @@
 
-s = '07:05:45PM'
+s = '12:05:45AM'
 
 time_split = s.split(":")
 print(time_split)
 if 'PM' in time_split[2]:
-    print("PM", time_split[2])
+    #print("PM", time_split[2])
+    if int(time_split[0]) < 12:
+        hr = int(time_split[0])+12
+        print(str(hr)+":"+time_split[1]+":"+time_split[2].replace('PM',''))
+    else:
+        print(time_split[0]+":"+time_split[1]+":"+time_split[2].replace('PM',''))
+
 else:
-    print("AM", time_split[2])
+    #print("AM", time_split[2])
+    if int(time_split[0]) < 12:
+        print(time_split[0]+":"+time_split[1]+":"+time_split[2].replace('AM',''))
+    else:
+        hr = int(time_split[0])-12
+        print(str(hr)+":"+time_split[1]+":"+time_split[2].replace('AM',''))
