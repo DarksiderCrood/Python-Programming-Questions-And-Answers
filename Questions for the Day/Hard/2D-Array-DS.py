3ab17dd657930af86f95ed85c4552d7e39b27052
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-24 19:52:56 +0530

    24 Sep 2021

--- Questions for the Day/Hard/2D-Array-DS.py
+++ Questions for the Day/Hard/2D-Array-DS.py
@@ -1,3 +1,14 @@
+import math
+
+
+def hourglass(arr):
+    max_num = -math.inf
+    for x in range(4):
+        for y in range(4):
+            sm = arr[x][y] + arr[x][y+1] + arr[x][y+2] + arr[x+1][y+1] + arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2]
+            max_num = max((max_num, sm))
+    print(max_num)
+
 arr = [
         [1, 1, 1, 0, 0, 0], 
         [0, 1, 0, 0, 0, 0], 
@@ -5,4 +16,4 @@ arr = [
         [0, 0, 2, 4, 4, 0], 
         [0, 0, 0, 2, 0, 0], 
         [0, 0, 1, 2, 4, 0]]
-print(arr)
+hourglass(arr)
