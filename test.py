2f38c69efde39287238493eac73993184d495aed
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-26 01:32:15 +0530

    26 Sep 2021

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-def pascal_triangle(arr, n):
-   row = [1]
-   y = [0]
-   arrstr = ''.join(str(k) for k in arr)
-   iarrl = len(arr)
-   for _ in range(max(n,0)):
-      rowstr = ''.join(str(k) for k in row)
-      if arrstr in rowstr:
-          print(row[iarrl])
-      row=[l+r for l,r in zip(row+y, y+row)]
-   return n>=1
-
-
-if __name__ == '__main__':
-    arr = [1,5] 
-    n = max(arr)+1
-    pascal_triangle(arr, n) 
-
+st = "(1 1) (8 8)"
+k = [int(x) for x in st if x.isnumeric()]
+knightpos = k[0:2]
+targetpos = k[2:4]
+print(knightpos,targetpos)
