edc24d7e745c5e3dfe1c00f0d975b2055fb24f0c
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-25 17:31:17 +0530

    25 Sep 2021

--- test.py
+++ test.py
@@ -1,22 +1,11 @@
-from itertools import islice
+from itertools import combinations
 
 
-s = "poTaTO" # pTo*Ta*O
-l = [x for x in s]
-lines = list(s)
-lit = iter(enumerate(lines))
-pos = []
-ad = []
+A = [15, -2, 0, -8, 3, 7, 10, 23]
+sub = []
 
-for x in range(0, len(l)):
-    if x < len(l)-1:
-        if l[x].islower() and l[x+1].isupper():
-            l[x], l[x+1] = l[x+1], l[x]
-            pos.append(x+1+1)
-            next(islice(l, 2), None)
-
-pos_inc = 0
-for i in pos:
-    l.insert(i+pos_inc, '*')
-    pos_inc+=1
-print(''.join(l))
+for x in range(0, len(A)+1):
+    temp = [list(i) for i in combinations(A, x)]
+    if len(temp)>0:
+        sub.extend(temp)
+print(sub)
