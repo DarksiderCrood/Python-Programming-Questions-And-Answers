4521282bb134d9774284d5ab148878273f8c5b00
DarksiderCrood <jhakaushtubh@gmail.com>
2022-02-15 22:55:44 +0530

    15 Feb 2022

--- Questions for the Day/Easy/String/Anagram_String.py
+++ Questions for the Day/Easy/String/Anagram_String.py
@@ -1,24 +1,25 @@
 def remAnagram(str1,str2):
     
     #add code here
-    if len(str1) > len(str2):
+    if len(str1) < len(str2):
+        count = 0
+        for x in str1:
+            if x in str2:
+                count +=1
+        if count == len(str1):
+            return len(str2) - count
+    else:
         count = 0
         for x in str2:
-            if not x in str1:
+            if x in str1:
                 count +=1
-        print(count)
-    else:
-        for x in str1:
-            if not x in str2:
-                count +=1
-        print(count)
+        if count == len(str2):
+            return len(str1) - count
 
 #{ 
 #  Driver Code Starts
 if __name__=='__main__':
-    t=int(input())
-    for i in range(t):
-        str1=input()
-        str2=input()
-        print(remAnagram(str1,str2))
+    str1 = 'basgadhbfgvhads'
+    str2 = 'sjdhgvbjdsbhvbvd'
+    print(remAnagram(str1,str2))
 # } Driver Code Ends
