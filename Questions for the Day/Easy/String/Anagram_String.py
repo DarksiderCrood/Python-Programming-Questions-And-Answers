5c2b50e34050fb328c90dd87dbcfb40eeb5594fa
DarksiderCrood <jhakaushtubh@gmail.com>
2022-02-15 22:56:44 +0530

    15 Feb 2022

--- Questions for the Day/Easy/String/Anagram_String.py
+++ Questions for the Day/Easy/String/Anagram_String.py
@@ -1,3 +1,23 @@
+"""
+Anagram of String 
+
+Given two strings S1 and S2 in lowercase, the task is to make them anagram. 
+The only allowed operation is to remove a character from any string. 
+Find the minimum number of characters to be deleted to make both the strings anagram. 
+Two strings are called anagram of each other if one of them can be converted into another 
+by rearranging its letters.
+
+Example 1:
+
+Input:
+S1 = bcadeh
+S2 = hea
+Output: 3
+Explanation: We need to remove b, c
+and d from S1.
+"""
+
+
 def remAnagram(str1,str2):
     
     #add code here
