6273078f08f9ea04148905676828d9b3acdd9346
DarksiderCrood <jhakaushtubh@gmail.com>
2022-02-25 11:24:07 +0530

    25 Feb 2022

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-#max from list without inbuild
-l = [-1,-3,-4,-7,-2,-5,-7,-10,-5,-2]
-max = l[0]
-for i in l:
-    if max < i:
-        max = i
-print(max)
+st = 'AAAAAAA'
+
+if st == 'A*':
+    print('ok')
